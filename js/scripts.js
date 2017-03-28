a669121019a5483d612219a6a00cb6997b66b198
Adam <slayer@plusnet.pl>
2017-03-28 21:43:44 +0200

    8.8 Papugi i słonie

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach';
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+var textCharsAfter = text.replace('Papugi', animalUpperCased);
+
+
+
+//console.log(text);
+//console.log(animalUpperCased);
+//console.log(textCharsAfter.length/2);
+console.log(textCharsAfter.slice(0,45));
